d51496bf917482be1744aee64717f598aa9f4289
l3viathanofficial <85458572+l3viathanofficial@users.noreply.github.com>
2021-06-07 15:42:19 -0400

    Add files via upload

---
+++ src/text/contentEN.js
@@ -0,0 +1,90 @@
+
+
+var contentEN = {
+  "about" : {
+    "name" : "About ",
+    "content" : 
+<>
+<p>
+L3viathan BSC
+</p>
+L3viathan is coming to BSC in Q2 2021.
+<p>
+
+</p>
+It hopes to become comparable to the mythical beast by utilizing some well known and unforseen aspects on BSC.
+<p>
+Further information is available through navigating through the site, and on socials.
+</p>
+<p>
+It is recommended to both follow the socials and join the telegram group for information regarding L3viathan.
+</p>
+</>
+  },
+
+
+  "Tokenomics" : {
+    "name" : "Tokenomics", 
+    "content" : 
+<>
+<p>
+Total Supply: 23,000,000
+</p>
+<p>
+Burned: 10,350,000 (45% before launch)
+</p>
+<p>
+Whitelist Sale: 5,750,000 (25%)
+</p>
+<p>
+Liquidity: 5,750,000 (25% LPs will be locked on Dxsale)
+</p>
+<p>
+  Marketing: 920,000 (4% marketing tokens not used before launch will be locked and emit on vesting schedule)
+</p>
+<p>
+  Team: 230,000 (1%)
+</p>
+<p>Total tax per tx: 6% </p>
+<p>Total per tx to liquidity: 4%</p>
+<p>Total per tx for tax: 1%</p>
+<p>Total per tx reflection: 1%</p>
+<p><b>Additional Features/Information:</b> 45% burn before launch to create a blackhole to continue to grow taking more tokens out of circulation</p>
+<p>Marketing funds set aside to allow project to continue longer than a short amount of time.</p>
+<p>Contract will not be renounced but it will locked for 7 day periods</p>
+<p>Extremely active anti-bot/whale features will be utilized</p>
+<p>Scavenger hunt for whitelist spot to allow dedicated members in at the ground floor to create a strong foundation.</p>
+
+</>
+  },
+
+
+  "links" : {
+    "name" : "Links",
+    "content" : 
+<>
+<p>Listed below are links related to the project. If you have marketing or partnership inquiries please feel free to e-mail us or reach out in our telegram channel.</p>
+<p>Links:</p>
+<p> 🐦 <a rel="noreferrer" target="_blank" href="https://twitter.com/L3viathanB">Twitter</a></p>
+<p> 📰 <a rel="noreferrer" target="_blank" href="	https://medium.com/@l3viathan">Medium</a></p>
+<p> 💻 <a rel="noreferrer" target="_blank" href="https://github.com/l3viathanofficial">Github</a></p>
+<p> 👔 <a rel="noreferrer" target="_blank" href="https://www.bscscan.com">Contract/BSCScan</a></p>
+<p> ✈️ <a rel="noreferrer" target="_blank" href="https://t.me/l3viathanbsc">Telegram</a></p>
+<p> ✈️ <a rel="noreferrer" target="_blank" href="https://t.me/l3viathanannouncement">Telegram Announcements</a></p>
+<p> 📈 <a rel="noreferrer" target="_blank" href="https://www.dextools.io/app/bsc">Chart Dextools</a></p>
+<p> 📨 <a href="mailto:team@l3viathan.com">E-mail Us</a></p>
+</>
+  },
+  "Roadmap" : {
+    "name" : "Roadmap", 
+    "content" : 
+    <>
+    <p> Q2 2021 - Initial Stage(s)- Whitelist event, BSC Scan information verified/updated, initial relationship with marketers/influencers established,listing on coinhunt and coinsniper (before launch preferred), token launch</p>
+    <p> Q3 2021 - after becoming successfully establish further partnerships with established and emerging tokens on BSC</p>
+    <p>Q4 2021- TBD upon team deliberation and community feedback</p>
+    </>
+  }
+}
+
+
+export default contentEN
